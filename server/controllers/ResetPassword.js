e7f84ee575e86942f38178f168ea0f3fafffb495
keshavsharma74 <keshav165p@gmail.com>
2025-06-13 01:15:47 +0530

    Implement resetPassword functionality with validation and error handling

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -24,7 +24,7 @@ const resetPasswordToken = async (req,res) =>{
             {new:true}
         )
 
-        const url=`http://localhost:3000/update-password/${token}`
+        const url=`http://localhost:3000/update-password/${token}`;
 
         await mailSender(email,"Password Reset Link",`Password Reset Link : ${url}`);
 
@@ -42,3 +42,56 @@ const resetPasswordToken = async (req,res) =>{
         })
     }
 }
+
+const resetPassword = async (req, res) => {
+    try {
+          //data fetch
+        const {password, confirmPassword, token} = req.body;
+        //validation
+        if(password !== confirmPassword) {
+            return res.json({
+                success:false,
+                message:'Password not matching',
+            });
+        }
+        //get userdetails from db using token
+        const userDetails = await User.findOne({token: token});
+        //if no entry - invalid token
+        if(!userDetails) {
+            return res.json({
+                success:false,
+                message:'Token is invalid',
+            });
+        }
+        //token time check 
+        if( userDetails.resetPasswordExpires < Date.now()  ) {
+                return res.json({
+                    success:false,
+                    message:'Token is expired, please regenerate your token',
+                });
+        }
+        //hash pwd
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        //password update
+        await User.findOneAndUpdate(
+            {token:token},
+            {password:hashedPassword},
+            {new:true},
+        );
+        //return response
+        return res.status(200).json({
+            success:true,
+            message:'Password reset successful',
+        });
+    }
+    catch(error) {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:'Something went wrong while sending reset pwd mail'
+        })
+    }
+}
+
+module.exports = {resetPassword,resetPasswordToken}
